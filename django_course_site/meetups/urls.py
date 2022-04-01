f88c78d6fdffef0b737a2bfdeb605f8be7b19fdc
CarrascoJose <jmcotema@gmail.com>
2022-04-01 00:05:36 -0300

    Curso finalizado

--- django_course_site/meetups/urls.py
+++ django_course_site/meetups/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups/',views.index, name='all-meetups'), #our-domain.com/meetups
-    path('meetups/<meetup_slug>',views.meetup_details,name='meetup-detail'), #our-domain.com/meetups/<dynamic-path-segment>
-    path('meetups/confirmation/<meetup_slug>',views.confirmation_page,name='meetup-confirmation') #confirmation-page
+    path('',views.index, name='all-meetups'), #our-domain.com/meetups
+    path('<meetup_slug>',views.meetup_details,name='meetup-detail'), #our-domain.com/meetups/<dynamic-path-segment>
+    path('confirmation/<meetup_slug>',views.confirmation_page,name='meetup-confirmation') #confirmation-page
 ]
